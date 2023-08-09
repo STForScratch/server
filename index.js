6a1a5235655aa614de4560b8674fb7ef7d908183
rgantzos <86856959+rgantzos@users.noreply.github.com>
2023-08-08 22:14:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -570,8 +570,8 @@ app.get("/isbeta/:username/", async function (req, res) {
 
 app.get("/news/", async function(req, res) {
   res.send({
-    title: "ScratchTools v3!",
-    description: 'The latest version of ScratchTools is now here, with a major new release! You can now customize what your settings page looks like, or open up a live chat support session with a specialist! There is so much cool new stuff, check it out!'
+    title: "Now in Japanese!",
+    description: 'ScratchTools v3.1.0 will be available in Japanese! This is the first language that we have been fully translated into for a while, thanks to our massive localization team with over 100 members. Many other languages including Spanish, Polish, and Turkish are also very close to being completed.'
   })
 })
 
