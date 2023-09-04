35d3fc435364f62a0690f3c993ecf2ed8e42e8d0
rgantzos <86856959+rgantzos@users.noreply.github.com>
2023-09-04 14:56:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -251,30 +251,50 @@ let recentUninstalls = [];
 
 app.post("/uninstall/", jsonParser, async function (req, res) {
   if (req.body.server === process.env.server) {
-        {
-          name: "Installed",
-          value: `<t:${req.body.timeInstalled || "Unknown Time"}>`,
-          inline: false,
-        },
-        {
-          name: "Features Enabled Code",
-          inline: false,
-        },
-        {
-          name: "Version",
-          value: "`" + (req.body.version || "") + "`",
-          inline: false,
-        }
-      );
-    webhookClient.send({
-      username: "ScratchTools Webserver Moderation",
-      avatarURL:
-        "https://raw.githubusercontent.com/STForScratch/ScratchTools/main/extras/icons/beta/beta128.png",
-      embeds: [embed],
-    });
-    res.send({
-      success: true,
-    })
+    if (
+      !recentUninstalls.find(
+        (u) => u.ip === req.body.ip && u.time > (Date.now() - 43200000)
+      )
+    ) {
+      recentUninstalls.push({
+        ip: req.body.ip,
+        time: Date.now(),
+      });
+      let embed = new EmbedBuilder()
+        .setTitle("Uninstall")
+        .setDescription("A user has uninstalled ScratchTools.")
+        .addFields(
+          {
+            name: "Username (Not Verified)",
+            value: req.body.username || "Unknown Username",
+            inline: false,
+          },
+          {
+            name: "Installed",
+            value: `<t:${req.body.timeInstalled || "Unknown Time"}>`,
+            inline: false,
+          },
+          {
+            name: "Features Enabled Code",
+            value: "`" + (req.body.features || "Unknown") + "`",
+            inline: false,
+          },
+          {
+            name: "Version",
+            value: "`" + (req.body.version || "") + "`",
+            inline: false,
+          }
+        );
+      webhookClient.send({
+        username: "ScratchTools Webserver Moderation",
+        avatarURL:
+          "https://raw.githubusercontent.com/STForScratch/ScratchTools/main/extras/icons/beta/beta128.png",
+        embeds: [embed],
+      });
+      res.send({
+        success: true,
+      });
+    }
   }
 });
 
